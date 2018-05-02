b8d7c6d21eb84fd1a42a2b06496a996057c3a7b3
Tore Haglund <ytterdorr@gmail.com>
2018-05-02 09:36:40 +0200

    new state machine

---
+++ state_machine/state_machine_new_spa.py
@@ -0,0 +1,43 @@
+import nengo
+import nengo_spa as spa
+import numpy as np
+import matplotlib.pyplot as plt
+
+d = 32
+with spa.Network() as model:
+    scene = spa.Transcode('BLUE * CIRCLE + RED * SQUARE', output_vocab=d)
+    query = spa.Transcode(lambda t: 'CIRCLE' if t < 0.25 else 'SQUARE', output_vocab=d)
+    result = spa.State(vocab=d)
+
+    scene * ~query >> result
+
+    p = nengo.Probe(result.output, synapse=0.01)
+
+with nengo.Simulator(model) as sim:
+	sim.run(0.5)
+
+plt.plot(sim.trange(), spa.similarity(sim.data[p], result.vocab))
+plt.xlabel("Time [s]")
+plt.ylabel("Similarity")
+plt.legend(result.vocab, loc="best")
+
+plt.show()
+
+
+"""
+with spa.Network() as model:
+	stimulus = spa.Transcode('Hello', output_vocab=d)
+	state = spa.State(vocab=d)
+	nengo.Connection(stimulus.output, state.input)
+	p = nengo.Probe(state.output, synapse=0.01)
+
+with nengo.Simulator(model) as sim:
+	sim.run(0.5)
+
+plt.plot(sim.trange(), spa.similarity(sim.data[p], state.vocab))
+plt.xlabel("Time [s]")
+plt.ylabel("Similarity")
+plt.legend(state.vocab, loc="best")
+
+plt.show()
+"""
