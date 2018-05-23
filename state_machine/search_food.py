bb3656ff74e05c3f224643b97c17cb702cd27b77
Tore Haglund <ytterdorr@gmail.com>
2018-05-23 13:45:42 +0200

    cleaned up the code, I hope

--- state_machine/search_food.py
+++ state_machine/search_food.py
@@ -76,69 +76,12 @@ class State(nengo.Node):
         return what+where        
 
 
-
-
-def food_input(t):
-    if t < 1.5:
-        return "NOFOOD"
-    else:
-        return "FOOD"
-
-def starter(t):
-    if t < 0.05:
-        return "START"
-    else:
-        return "0"
-
-def check_turn(t, action):
-    turn = vocab.parse("TURN").v
-    if np.dot(action, turn) > 0.5:
-        return 1
-    else:
-        return 0
-
-def check_forward(t, action):
-    forward = vocab.parse("FORWARD").v
-    if np.dot(action, forward) > 0.5:
-        return 1
-    else:
-        return 0
-
-
-def turn_function(t, x):
-    return body.turn(x[0]*x[1]*0.005)
-
-def forward_function(t, x):
-    return body.go_forward(x*0.01)
-
-def get_r(sinCosArray):
-        sin = sinCosArray[0]
-        cos = sinCosArray[1]
-        r = np.sqrt(sin*sin+cos*cos)
-        return r
-    
-def stop_if_r_is_large(sinCosArray):
-    r_size = get_r(sinCosArray)
-    if r_size > 0.9:
-        return 1
-    else:
-        return 0
-
-def translate_food_state(t, food_is_close):
-    food = vocab.parse("FOOD").v
-    nofood = vocab.parse("NOFOOD").v
-    
-    if food_is_close > 0.5:
-        return food
-    else:
-        return nofood
-
 with model:
 
-    # Environment
+    ### Environment
     env = grid.GridNode(world)
 
-    # Perceptions
+    ### Perceptions
     sensors = State(body)
     what = nengo.networks.EnsembleArray(n_ensembles=sensors.n_objects,
                                         ens_dimensions=1,
@@ -150,7 +93,7 @@ with model:
     nengo.Connection(sensors[:sensors.n_objects], what.input, synapse=None)
     nengo.Connection(sensors[sensors.n_objects:], where.input, synapse=None)
     
-    # SPA State machine
+    ### SPA State machine
     model.subgoal = spa.State(D)
     model.action_state = spa.State(D, vocab=vocab, feedback=1, feedback_synapse=0.01)
     model.food_percept = spa.State(D, vocab=vocab, feedback=1, feedback_synapse=0.01)
@@ -166,22 +109,35 @@ with model:
         'dot(action_state, WAIT2*0.6) -2*dot(food_percept, FOOD) --> action_state=FORWARD',
         'dot(action_state, FORWARD*0.6) -2*dot(food_percept, FOOD) --> action_state=CHECK1',
         'dot(food_percept, FOOD) --> action_state=STOP, subgoal=SEEK_FOOD',
-        '0.5 --> ',
+        #'0.5 --> ',
         )
 
     model.bg = spa.BasalGanglia(actions=actions)
     model.thal = spa.Thalamus(model.bg)
 
-    ## Inputs and outputs
-    #model.food_input = spa.Input(food_state=food_input)
+    ### Start signal
+    def starter(t):
+        if t < 0.05:
+            return "START"
+        else:
+            return "0"
+    
     model.start_input = spa.Input(action_state=starter)
 
-    # Turning
-    check_turn_node = nengo.Node(check_turn, size_in=D)
-    turn_ensemble = nengo.Ensemble(n_neurons=200, dimensions=2)
-    turn_node = nengo.Node(turn_function, size_in=2)
+    
+    ### Turning
+    
+    def check_turn(t, action):
+        turn = vocab.parse("TURN").v
+        if np.dot(action, turn) > 0.5:
+            return 1
+        else:
+            return 0
 
-    # Now let's implement the simplest possible behaviour: turn towards objects.
+    def turn_function(t, x):
+        return body.turn(x[0]*x[1]*0.005)
+
+    # Terry's turn towards function
     def turn_towards(x):
         if x[0] < -0.1:
             return 1
@@ -189,26 +145,70 @@ with model:
             return -1
         else:
             return 0
-    # do the above function for all the objects
+    
+    check_turn_node = nengo.Node(check_turn, size_in=D)
+    turn_ensemble = nengo.Ensemble(n_neurons=200, dimensions=2)
+    turn_node = nengo.Node(turn_function, size_in=2)
+
+    # How does this give a contionuous value?
     for i in range(sensors.n_objects):
         nengo.Connection(where.ensembles[i], turn_ensemble[1], function=turn_towards)
+    
     nengo.Connection(model.action_state.output, check_turn_node)
     nengo.Connection(check_turn_node, turn_ensemble[0])
     nengo.Connection(turn_ensemble, turn_node)
 
 
 
-    # Going forward
+    ### Going forward
+
+    def check_forward(t, action):
+        forward = vocab.parse("FORWARD").v
+        if np.dot(action, forward) > 0.5:
+            return 1
+        else:
+            return 0
+
+    def forward_function(t, x):
+        return body.go_forward(x*0.01)
+
     check_forward_node = nengo.Node(check_forward, size_in=D)
     forward_node = nengo.Node(forward_function, size_in=1)
     nengo.Connection(model.action_state.output, check_forward_node)
     nengo.Connection(check_forward_node, forward_node)
 
-    # For controlling the food input
+    
+    ### Controlling the food input
+
+    def get_r(sinCosArray):
+        sin = sinCosArray[0]
+        cos = sinCosArray[1]
+        r = np.sqrt(sin*sin+cos*cos)
+        return r
+    
+    def stop_if_r_is_large(sinCosArray):
+        r_size = get_r(sinCosArray)
+        if r_size > 0.9:
+            return 1
+        else:
+            return 0
+
+    def translate_food_state(t, food_is_close):
+        food = vocab.parse("FOOD").v
+        nofood = vocab.parse("NOFOOD").v
+        
+        if food_is_close > 0.5:
+            return food
+        else:
+            return nofood
+
     close_to_food = nengo.Ensemble(n_neurons=50, dimensions=1)
-    food_node = nengo.Node(translate_food_state, size_in=1)
+    # node to send food state vector based on close_to_food ensemble output 
+    food_node = nengo.Node(translate_food_state, size_in=1) 
+    
+    # How does this give a continuous value?
     for i in range(len(where.ensembles)):
         nengo.Connection(where.ensembles[i], close_to_food, function=stop_if_r_is_large)
+    
     nengo.Connection(close_to_food, food_node)
-
     nengo.Connection(food_node, model.food_state.input)
