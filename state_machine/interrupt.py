bd10e0223c0ecc149a182397cbe79a90903e6dca
Tore Haglund <ytterdorr@gmail.com>
2018-05-28 16:42:53 +0200

    first interrupt implemented!

---
+++ state_machine/interrupt.py
@@ -0,0 +1,89 @@
+import nengo
+import nengo.spa as spa
+import numpy as np
+
+
+D = 64
+vocab = spa.Vocabulary(D)
+food_vocab = spa.Vocabulary(D)
+goal_vocab = spa.Vocabulary(D)
+inter_vocab = spa.Vocabulary(D)
+
+model = spa.SPA(label="Find Food")
+
+with model:
+
+	def starter(t):
+		if t < 0.05:
+			return "START_FIND_FOOD"
+		else:
+			return "0"
+
+	def food_exist_func(t):
+		if t < 0.5:
+			return "NO_FOOD"
+		else:
+			return "FOOD"
+
+	def food_close_func(t):
+		if t < 1:
+			return "NO_FOOD"
+		else:
+			return "CLOSE"
+
+	def interrupt_input(t):
+		if t < 0.3:
+			return "0"
+		elif t < 0.4:
+			return "INTERRUPT"
+		else: 
+			return "0"
+
+	model.find_state = spa.State(dimensions=D, vocab=vocab, feedback=1, feedback_synapse=0.01)
+	model.go_state = spa.State(dimensions=D, vocab=vocab, feedback=1, feedback_synapse=0.01)
+	model.perception = spa.State(dimensions=D, vocab=vocab, feedback=1, feedback_synapse=0.05)
+	model.subgoal = spa.State(dimensions=D, vocab=goal_vocab, feedback=1, feedback_synapse=0.01)
+	model.subgoal_temp = spa.State(dimensions=D, vocab=goal_vocab, feedback=1, feedback_synapse=0.01)
+	model.food_exists = spa.State(dimensions=D, vocab=vocab)
+	model.food_close = spa.State(dimensions=D, vocab=vocab)
+	model.interrupt = spa.State(dimensions=D, vocab=vocab, feedback=1, feedback_synapse=0.01)
+
+	actions = spa.Actions(
+		# What is the stop condition?
+		### FIND FOOD 
+		'dot(subgoal, START_FIND_FOOD) --> find_state=START, subgoal=FIND_FOOD, go_state=STOP, subgoal_temp=START_FIND_FOOD',
+		'dot(find_state, START) - dot(perception, FOOD) -2*dot(interrupt, INTERRUPT) --> find_state=CHECK1',
+		'dot(find_state, CHECK1) - dot(perception, FOOD) -2*dot(interrupt, INTERRUPT) --> perception=food_exists, find_state=WAIT1',
+		'dot(find_state, WAIT1) - dot(perception, FOOD) -2*dot(interrupt, INTERRUPT) --> find_state=TURN',
+		'dot(find_state, TURN) - dot(perception, FOOD) -2*dot(interrupt, INTERRUPT) --> find_state=CHECK2',
+		'dot(find_state, CHECK2) - dot(perception, FOOD) -2*dot(interrupt, INTERRUPT) --> perception=food_exists, find_state=WAIT2',
+		'dot(find_state, WAIT2) - dot(perception, FOOD) -2*dot(interrupt, INTERRUPT) --> find_state=FORWARD',
+		'dot(find_state, FORWARD) - dot(perception, FOOD) -2*dot(interrupt, INTERRUPT) --> find_state=CHECK1',
+		# Stop condition
+		'dot(perception, FOOD) --> find_state=STOP, subgoal=START_GO_TO_FOOD, subgoal_temp=START_GO_TO_FOOD',
+		### GO TO FOOD
+		'dot(subgoal, START_GO_TO_FOOD) --> go_state=START, subgoal=GO_TO_FOOD, find_state=STOP, subgoal_temp=START_GO_TO_FOOD',
+		'dot(go_state, START) --> go_state=CHECK1',
+		'dot(go_state, CHECK1) --> go_state=WAIT1, perception=food_close',
+		'dot(go_state, WAIT1) - dot(perception, CLOSE) --> go_state=TURN',
+		'dot(go_state, TURN) - dot(perception, CLOSE) --> go_state=FORWARD',
+		'dot(go_state, FORWARD) - dot(perception, CLOSE) --> go_state=CHECK1',
+		# Stop condition
+		'dot(perception, CLOSE) --> go_state=STOP',
+		### Interrupt
+		'dot(interrupt, INTERRUPT) --> go_state=STOP, find_state=STOP, interrupt=CALC_TURN',
+		'dot(interrupt, CALC_TURN) --> interrupt=TURN1',
+		'dot(interrupt, TURN1) --> interrupt=TURN2',
+		'dot(interrupt, TURN2) --> interrupt=TURN3',
+		'dot(interrupt, TURN3) --> interrupt=FORWARD',
+		'dot(interrupt, FORWARD) --> interrupt=STOP, subgoal=subgoal_temp',
+
+		) 
+	model.bg = spa.BasalGanglia(actions=actions)
+	model.thal = spa.Thalamus(model.bg)
+
+	# Input
+	model.start_input = spa.Input(subgoal=starter)
+	model.exists_input = spa.Input(food_exists=food_exist_func)
+	model.food_close_input = spa.Input(food_close=food_close_func)
+	model.interrupt_input = spa.Input(interrupt=interrupt_input)
